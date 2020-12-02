4e2d723a130a80cb02412cf10839b070f9320405
anukul saini <anukulsaini2295@gmail.com>
2020-12-02 11:58:25 +0000

    a

---
+++ app.js
@@ -0,0 +1,52 @@
+ var express               = require("express"),
+	 methodOverride       =require("method-override"),
+     mongoose              = require("mongoose"),
+	 bodyParser            = require("body-parser"),
+    app                   = express(),
+	Comment               = require("./models/comment"),   
+	Campground            = require("./models/campground"),
+	passport              = require("passport"),
+	localStrategy         = require("passport-local"),
+	passportLocalMongoose = require("passport-local-mongoose"),
+	User                  = require("./models/user")
+
+var indexRoutes      =require("./routes/authentication"),   
+    commentRoutes    =require("./routes/comments"),
+	campgroundRoutes =require("./routes/campgrounds")
+
+
+
+
+mongoose.connect("mongodb://localhost:27017/Camping",{useNewUrlParser:true});
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
+//PASSPORT CONFIGURATION
+
+
+
+// we need these two following lines when we use passport.js
+app.use(passport.initialize());
+app.use(passport.session());
+
+// responsible for decoding and incoding data ..in the user module we have added passport-local-mongoose due tow hich these two works
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+// app.use("/campgrounds",campgroundRoutes);
+app.use("/",indexRoutes);
+// app.use("/campgrounds/:id/comments",commentRoutes);
+
+app.use(function(req,res,next){
+	res.locals.currentUser=req.user;
+	next();
+});
+
+app.listen(3000,function(){
+	console.log('SERVER LISTENING');
+});
---
+++ routes/authentication.js
@@ -0,0 +1,54 @@
+var express           = require("express"),
+router                = express.Router(),
+User                  = require("../models/user"),
+passport              = require("passport"),
+localStrategy         = require("passport-local"),
+passportLocalMongoose = require("passport-local-mongoose")
+	
+
+router.get("/",function(req,res){
+	res.render("landing");
+});
+router.get("/register",function(req,res){
+	res.render("register");
+});
+router.post("/register",function(req,res){
+	req.body.username
+	req.body.password
+	var newUser=new User({username:req.body.username})
+	User.register(newUser, req.body.password,function(err,user){
+		if (err){
+			// req.flash("error",err.message);
+			return res.render("register");
+		}
+		{
+			passport.authenticate("local")(req,res,function(){
+				// req.flash("success","Welcome to Yelpcamp " + user.username);
+				res.redirect("/campgrounds");
+			});
+		}
+	});
+});
+router.get("/login",function(req,res){
+	res.render("login");
+});
+//using middleware in next route
+router.post("/login",passport.authenticate("local",{
+	successRedirect:"/campgrounds",
+	failureRedirect:"/login"
+}),function(req,res){
+});
+router.get("/logout",function(req,res){
+	req.logout();
+	// req.flash("success","Successfully loged you out");
+	res.redirect("/");
+});
+function isLoggedIn(req,res,next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	// req.flash("error","please log in first!!")
+	res.redirect("/login");
+}
+
+module.exports = router;
